7214262d8e621b2275abdaaa2292e97820d15df8
adamwisnup <wisnuadam21@gmail.com>
2022-03-13 09:38:41 +0700

    first commit

---
+++ app/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use softDeletes;
+
+    protected $fillable = [
+        'name', 'photo', 'slug'
+    ];
+
+    protected $hidden = [];
+}
